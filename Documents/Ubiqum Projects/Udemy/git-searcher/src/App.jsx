8a30e5e3ed4fe52ed7c3af504ae34dedeb6ee279
Kazu Onishi <kmwa20042000@gmail.com>
2020-06-29 22:57:57 +0800

    added hooks, context, pages

--- Documents/Ubiqum Projects/Udemy/git-searcher/src/App.jsx
+++ Documents/Ubiqum Projects/Udemy/git-searcher/src/App.jsx
@@ -1,44 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
-import axios from 'axios';
-import Search from './components/users/Serach';
+import Alert from './components/layout/Alert';
+import About from './components/pages/About';
+import GithubState from './context/github/GithubState';
+import AlertState from './context/alert/AlertState';
+import User from './components/users/User';
+import Home from './components/pages/Home';
+import NotFound from './components/pages/NotFound';
 import './App.css';
 
-class App extends Component {
-  state = {
-    users: [],
-    loading: false,
-  };
+const App = () => {
+  //Get user info
 
-  async componentDidMount() {
-    this.setState({ loading: true });
-    const res = await axios.get(
-      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_SECRET}`
-    );
-    this.setState({ users: res.data, loading: false });
-  }
-  //Search users, props passed up from Search comp
-  searchUsers = async (text) => {
-    this.setState({ loading: true });
-
-    const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_SECRET}`
-    );
-    this.setState({ users: res.data.items, loading: false });
-  };
-
-  render() {
-    return (
-      <div className='App'>
-        <Navbar title='Github Finder' icon='fab fa-github' />
-        <div className='container'>
-          <Search searchUsers={this.searchUsers} />
-          <Users loading={this.state.loading} users={this.state.users} />
-        </div>
-      </div>
-    );
-  }
-}
+  return (
+    <GithubState>
+      <AlertState>
+        <Router>
+          <div className='App'>
+            <Navbar title='Github Finder' icon='fab fa-github' />
+            <div className='container'>
+              <Alert />
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/about' component={About} />
+                <Route exact path='/user/:login' component={User} />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </AlertState>
+    </GithubState>
+  );
+};
 
 export default App;
